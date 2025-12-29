147da47ff9858cb54d334c26819e7d555fec0a3a
Dat Vo <vothanhdat@outlook.com>
2025-12-29 13:10:27 +0700

    add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'path'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ 
-      include: ['src'], 
-      insertTypesEntry: true,
-      tsconfigPath: './tsconfig.app.json',
-      rollupTypes: true,
-    })
-  ],
+  plugins: [react(), dts({
+    include: ['src'],
+    insertTypesEntry: true,
+    tsconfigPath: './tsconfig.app.json',
+    rollupTypes: true
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'ReactUiCustom',
       formats: ['es', 'umd'],
-      fileName: (format) => `react-ui-custom.${format}.js`,
+      fileName: format => `react-ui-custom.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
@@ -27,9 +32,32 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'jsxRuntime',
-        },
-      },
-    },
+          'react/jsx-runtime': 'jsxRuntime'
+        }
+      }
+    }
   },
-})
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
